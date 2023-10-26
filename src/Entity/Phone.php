b8630f8dfad97d4c0944ad639a8561624416416a
mdelacre <mdelacre@protonmail.com>
2023-10-26 21:57:31 +0200

    création de la première entité: phone

---
+++ src/Entity/Phone.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PhoneRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PhoneRepository::class)]
+class Phone
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $model = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $marque = null;
+
+    #[ORM\Column(length: 10)]
+    private ?string $battery = null;
+
+    #[ORM\Column]
+    private array $colors = [];
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getModel(): ?string
+    {
+        return $this->model;
+    }
+
+    public function setModel(string $model): static
+    {
+        $this->model = $model;
+
+        return $this;
+    }
+
+    public function getMarque(): ?string
+    {
+        return $this->marque;
+    }
+
+    public function setMarque(string $marque): static
+    {
+        $this->marque = $marque;
+
+        return $this;
+    }
+
+    public function getBattery(): ?string
+    {
+        return $this->battery;
+    }
+
+    public function setBattery(string $battery): static
+    {
+        $this->battery = $battery;
+
+        return $this;
+    }
+
+    public function getColors(): array
+    {
+        return $this->colors;
+    }
+
+    public function setColors(array $colors): static
+    {
+        $this->colors = $colors;
+
+        return $this;
+    }
+}
