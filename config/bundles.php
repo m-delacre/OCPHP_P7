3d73bfb47fc1dbf00a20cd93436cc93dc984afa1
mdelacre <mdelacre@protonmail.com>
2023-10-27 02:20:28 +0200

    ajout de la connexion avec les tokens JWT

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     Bazinga\Bundle\HateoasBundle\BazingaHateoasBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
