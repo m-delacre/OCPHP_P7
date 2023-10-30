7d440d022a5c58b70d8ad0f9b52dc9d59dde2cc7
mdelacre <mdelacre@protonmail.com>
2023-10-30 12:22:49 +0100

    ajout de la route pour avoir les détails d'un client, ajout de l'autodécouvrabilité

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Company;
 use App\Entity\Client;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\Id;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,6 +13,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use JMS\Serializer\SerializerInterface;
 use JMS\Serializer\SerializationContext;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class ClientController extends AbstractController
 {
@@ -31,8 +33,29 @@ class ClientController extends AbstractController
         return new JsonResponse($jsonClientList, Response::HTTP_OK, [], true);
     }
 
+    #[Route('/api/clients/{id}', name: 'api_client_details', methods: ['GET'])]
+    public function getClientDetailst(Client $client, SerializerInterface $serializer): JsonResponse
+    {
+        /**
+         * @var Company
+         */
+        $user = $this->getUser();
+
+        if ($client) {
+            if ($client->getCompany() === $user) {
+                $context = SerializationContext::create()->setGroups(['getSingleClient']);
+                $jsonClient = $serializer->serialize($client, 'json', $context);
+                return new JsonResponse($jsonClient, Response::HTTP_OK, [], true);
+            } else {
+                return new JsonResponse(null, Response::HTTP_UNAUTHORIZED);
+            }
+           
+        }
+        return new JsonResponse(null, Response::HTTP_NOT_FOUND);
+    }
+
     #[Route('/api/clients', name: 'api_client_create', methods: ['POST'])]
-    public function createClient(Request $request, SerializerInterface $serializer, EntityManagerInterface $em): JsonResponse
+    public function createClient(UrlGeneratorInterface $urlGenerator, Request $request, SerializerInterface $serializer, EntityManagerInterface $em): JsonResponse
     {
         $client = $serializer->deserialize($request->getContent(), Client::class, "json");
         $client->setCompany($this->getUser());
@@ -42,6 +65,8 @@ class ClientController extends AbstractController
         $context = SerializationContext::create()->setGroups(['getSingleClient']);
         $jsonClient = $serializer->serialize($client, 'json', $context);
 
-        return new JsonResponse($jsonClient, Response::HTTP_OK, [], true);
+        $location = $urlGenerator->generate('api_client_details', ['id' => $client->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
+        
+        return new JsonResponse($jsonClient, Response::HTTP_CREATED, ["Location"=>$location], true);
     }
 }
